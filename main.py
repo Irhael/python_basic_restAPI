c994ec0db36be31769c3844280c3797a8ab879d2
Irhael Sousa das Chagas <irhaelchagas@id.uff.br>
2023-11-09 18:33:10 -0300

    feat(main) Add override for post method, now has a response.

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ api = Api(app) # This line is used to initialize the flask_restful api
 class Route(Resource): # this class is used to create a resource. It will work like a route with methods that can be overrided.
     def get(self): # This method is used to send a get request to the server
         return {"data": "This is a get method"} # This line is used to return a json object to the client
+    
+    def post(self):
+        return {"data": "This is a post method"}
 
 api.add_resource(Route, "/route")
 # This line is used to add the resource created below to the api. The first parameter is the resource class and the second is the route, that will be used to access the resource.
